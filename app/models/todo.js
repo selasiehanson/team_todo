1447414ba76fbc7ee8ecd7ec28d90f92a9940a07
Selasie Hanson <selasie.hanson@andela.com>
2017-06-07 11:25:23 +0000

    WIP initial commit with basic tests

---
+++ app/models/todo.js
@@ -0,0 +1,6 @@
+import DS from "ember-data";
+
+export default DS.Model.extend({
+  text: DS.attr("String"),
+  completed: DS.attr("boolean")
+});
---
+++ tests/acceptance/todos-test.js
@@ -0,0 +1,36 @@
+import { test } from "qunit";
+import moduleForAcceptance from "team-todo/tests/helpers/module-for-acceptance";
+
+moduleForAcceptance("Acceptance | todos", {
+  beforeEach() {
+    visit("/todos");
+  }
+});
+
+test("visiting /todos", assert => {
+  andThen(() => {
+    assert.equal(currentURL(), "/todos");
+  });
+});
+
+test("should show welcome message", assert => {
+  andThen(() => {
+    assert.equal(find(".header").text(), "Welcome");
+  });
+});
+
+test("should show a form with input field", assert => {
+  andThen(() => {
+    assert.equal(find("input.new-todo").val(), "");
+  });
+});
+
+test("should show an add button", assert => {
+  andThen(() => assert.equal(find("button.submit").text(), "Add Todo"));
+});
+
+test("should show newly added todo to top of todos", assert => {
+  fillIn("input.new-todo", "Skate");
+  click("button.submit");
+  andThen(() => assert.equal(find("ul.todos li:first").text(), "Skate"));
+});
